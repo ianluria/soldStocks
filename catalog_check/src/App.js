73dbd2320b4681217b40129e7806c5c9710d7f61
Ian Luria <ian.luria@gmail.com>
2020-08-10 22:43:08 -0600

    Adding front end code for adding errors to be fixed.

--- catalog_check/src/App.js
+++ catalog_check/src/App.js
@@ -4,6 +4,7 @@ import { faAngleDown, faFireExtinguisher } from '@fortawesome/free-solid-svg-ico
 
 import ListGroup from 'react-bootstrap/ListGroup';
 import Collapse from 'react-bootstrap/Collapse';
+import Form from 'react-bootstrap/Form';
 
 // import './App.css';
 
@@ -42,18 +43,60 @@ function App() {
 function DisplayCatalogErrors(props) {
 
   const [open, setOpen] = useState({});
+  const [fixErrors, setFixErrors] = useState({});
 
+  // Expand collapse list item when 'parent' list item is clicked
   function handleListItemClick(e) {
-
     const updatedOpenCollapse = Object.assign({}, open);
     if (updatedOpenCollapse.hasOwnProperty(e.currentTarget.id)) {
       updatedOpenCollapse[e.currentTarget.id] = !updatedOpenCollapse[e.currentTarget.id];
     } else {
       updatedOpenCollapse[e.currentTarget.id] = true;
     }
-
     setOpen(updatedOpenCollapse)
     return;
+  }
+
+  // Add or remove the error type to/from object of error fixes on switch press
+  function handleCollapseSwitch(e) {
+    const updatedFixErrors = Object.assign({}, fixErrors);
+    if (updatedFixErrors.hasOwnProperty(e.currentTarget.id)) {
+      updatedFixErrors[e.currentTarget.id] = !updatedFixErrors[e.currentTarget.id];
+    } else {
+      updatedFixErrors[e.currentTarget.id] = true;
+    }
+    setFixErrors(updatedFixErrors)
+    return;
+  }
+
+  // Send list of errors to fix to backend
+  function handleErrorCorrectionSubmit(e) {
+
+    if (fixErrors) {
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(fixErrors)
+      };
+
+      fetch('/errorFix', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+
+        });
+    }
+  }
+
+  const errorMessages = {
+    dateError: "Convert date to YYYY-MM-DD format.",
+    titleError: "Get updated title from Keepa.",
+    brandError: "Get updated brand from Keepa.",
+    manufacturerError: "Get updated manufacturer from Keepa.",
+    tldError: "Fill in blank TLD fields using the catalog's TLD.",
+    retailerError: "Add Amazon add retailer."
   }
 
   const errorsArray = props.catalogErrors.slice();
@@ -78,7 +121,13 @@ function DisplayCatalogErrors(props) {
           <Collapse id={errorsArray[row].listKey + "Collapse"} in={open[errorsArray[row].listKey]}>
             <div>
               <ListGroup.Item>
-                This is where error fix information will be present.
+                <Form.Check
+                  type="switch"
+                  id="custom-switch"
+                  label="Check this switch"
+                  onClick={handleCollapseSwitch}
+                />
+                {errorMessages[errorsArray[row].listKey]}
               </ListGroup.Item>
             </div>
           </Collapse>
